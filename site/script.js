23797805e1261f117df51f553a9e226a222478c3
unknown <rafaeldfnrafa@gmail.com>
2024-04-21 21:12:45 -0400

    primeiro teste

---
+++ site/script.js
@@ -0,0 +1,11 @@
+// const button = document.querySelector("header button")
+
+// button.addEventListener("click", add)
+// function add(){
+
+//     let img = document.createElement("isa.jpg");
+//     img.scr= isa.jpg;
+//     document.body.appendChild(img);
+
+// }
+document.getElementById(imagem).innerHTML = "<img src='site/isa.jpg'   ";
